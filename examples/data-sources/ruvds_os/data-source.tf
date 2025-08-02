06dbac88c75c49143ef06a072bb45ae2e6c1173f
rustam <rustam.kulenov@gmail.com>
2025-08-03 00:56:36 +0300

    Examples updated

--- examples/data-sources/ruvds_os/data-source.tf
+++ examples/data-sources/ruvds_os/data-source.tf
@@ -8,3 +8,12 @@ output "os_ubuntu_2204" {
 
 # Gets information about a specific OS.
 # Output:
+#  + os_ubuntu_2204  = {
+#      + code               = "53-ubuntu-22.04-lts-eng"
+#      + id                 = 53
+#      + is_active          = true
+#      + name               = "Ubuntu 22.04 LTS (ENG)"
+#      + ssh_keys_supported = true
+#      + type               = "linux"
+#      + with_code          = "53-ubuntu-22.04-lts-eng"
+#    }
